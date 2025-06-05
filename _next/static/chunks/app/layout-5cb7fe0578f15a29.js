ad3b174a5b93ff9433b50c493976748ab0ee1812
noname1579 <ilyalashenko115@gmail.com>
2025-06-05 07:29:04 +0300

    Updates

--- _next/static/chunks/app/layout-5cb7fe0578f15a29.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{5900:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,9195,23))},9195:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[450,441,684,358],()=>s(5900)),_N_E=e.O()}]);
